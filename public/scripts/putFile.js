62102306c9ddeea4c27cb1849c50c07b6526b704
Adam <apalm112@gmail.com>
2019-08-05 17:35:51 -0400

    Started on wiring up write/read files to/from storage hub.

---
+++ public/scripts/putFile.js
@@ -0,0 +1,11 @@
+'use strict';
+
+document.addEventListener("DOMContentLoaded", event => {
+
+
+  document.getElementsByClassName('save-file-btn').addEventListener('click', event => {
+    event.preventDefault();
+    console.log('==================================================');
+    // userSession.putFile(path: string, content: string);
+  });
+});
