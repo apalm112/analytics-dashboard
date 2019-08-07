a6421abcdd4a0cacba3d01afb5677e27c2afedf1
Adam <apalm112@gmail.com>
2019-08-07 11:12:05 -0400

    Created file for function to use .getFile() method for Gaia Storage Hub.

--- public/scripts/putFile.js
+++ public/scripts/putFile.js
@@ -7,30 +7,25 @@ document.addEventListener("DOMContentLoaded", event => {
   var handleSaveDraft = document.getElementsByClassName('save-file-btn')[0];
   handleSaveDraft.addEventListener('click', event => {
     event.preventDefault();
-    // userSession.putFile(path: string, content: string);
     /*
-      div id='editor-container' get that element, grab text from it & pass to .putFile() method
-      has multiple children, each line is a separate <p></p>
-
       var userSession = new UserSession()
       let options = {
         encrypt: true
       }
       userSession.putFile("/message.txt", "Secret hello!", options)
       .then(() => {
-          // message.txt exists now, and has the contents "hello world!".
+        // message.txt exists now, and has the contents "hello world!".
       })
-
     */
+  // div id='editor-container' get that element, grab text from it & pass to .putFile() method, has multiple children, each line is a separate <p></p>
     var handleGetSaveDraftText = document.getElementById('editor-container');
     var getText = handleGetSaveDraftText.children[0].innerText;
-    // var userSession = new UserSession()
     let options = {
       encrypt: true
     }
     userSession.putFile("/saveDraftText.txt", getText, options)
       .then(() => {
-        // message.txt exists now, and has the contents "hello world!".
+        // saveDraftText.txt exists now, and has the contents of getText variable..
       })
 
     console.log(getText);
