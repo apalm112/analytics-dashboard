54e512fd435bcad1878773cc66c8a563fc1e7cf4
Adam <apalm112@gmail.com>
2019-08-12 14:53:50 -0400

    changes

--- server.js
+++ server.js
@@ -1,9 +1,9 @@
-const express = require('express');
+// const express = require('express');
 const open = require('open');
 // const router = require('./routes.js');
-const createError = require('http-errors');
+// const createError = require('http-errors');
 const path = require('path');
-const cookieParser = require('cookie-parser');
+// const cookieParser = require('cookie-parser');
 const logger = require('morgan');
 
 const app = express();
@@ -24,7 +24,7 @@ app.use(allowCrossDomain);
 app.use(logger('dev'));
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
-app.use(cookieParser());
+// app.use(cookieParser());
 // app.use('/', express.static(`${__dirname}/public`));
 app.use(express.static(path.join(__dirname, 'public')));
 
